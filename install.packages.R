8170532d8bd1eabe7788771109daed5e3f50ffc0
Ubuntu <ubuntu@ip-172-31-76-112.ec2.internal>
2018-10-08 17:06:03 +0000

    update Dockerfile and install.pacakges.R

---
+++ install.packages.R
@@ -0,0 +1,4 @@
+install.packages(c("MCMCpack","mvtnorm","ellipse","coda","Matrix","Rtsne","gtools","foreach","doParallel","doSNOW","snow","lattice","MASS","bayesm","robustbase","chron","mnormt"
+,"schoolmath","devtools","RColorBrewer"), dependencies=TRUE)
+#source("https://bioconductor.org/biocLite.R")
+#biocLite()
